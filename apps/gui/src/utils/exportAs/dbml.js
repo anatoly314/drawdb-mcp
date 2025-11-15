ec502b425c88d55543e19857965324d5aef012d4
1ilit <1ilit@proton.me>
2025-11-15 21:25:13 +0400

    Fix dbml comment export (#699)
    (cherry picked from commit c4e57596c4bf94880d082a55d364a1885dd7054e)

--- apps/gui/src/utils/exportAs/dbml.js
+++ apps/gui/src/utils/exportAs/dbml.js
@@ -40,14 +40,6 @@ function columnDefault(field, database) {
   return `default: ${parseDefaultDbml(field, database)}`;
 }
 
-function columnComment(field) {
-  if (!field.comment || field.comment.trim() === "") {
-    return "";
-  }
-
-  return `note: '${escapeQuotes(field.comment)}'`;
-}
-
 function columnSettings(field, database) {
   let constraints = [];
 
@@ -96,6 +88,23 @@ function processComment(comment) {
   }
 
   return `'${escapeQuotes(comment)}'`;
+}
+
+function columnComment(field) {
+  if (!field.comment || field.comment.trim() === "") {
+    return "";
+  }
+
+  return `note: ${processComment(field.comment)}`;
+}
+
+function processType(type) {
+  // TODO: remove after a while
+  if (type.toUpperCase() === "TIMESTAMP WITH TIME ZONE") {
+    return "timestamptz";
+  }
+
+  return type.toLowerCase();
 }
 
 export function toDBML(diagram) {
@@ -154,7 +163,7 @@ export function toDBML(diagram) {
               `\t${quoteIdentifier(field.name)} ${
                 field.type === "ENUM" || field.type === "SET"
                   ? quoteIdentifier(`${field.name}_${field.values.join("_")}_t`)
-                  : field.type.toLowerCase()
+                  : processType(field.type)
               }${fieldSize(
                 field,
                 diagram.database,
